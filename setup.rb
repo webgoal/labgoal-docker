4fccb2281181a77929a7e23475def91d9c05d5e3
Canedo <canedo@webgoal.com.br>
2019-01-25 15:03:51 -0200

    remove arquivo de setup desnecessario

--- setup.rb
+++
@@ -1,4 +0,0 @@
-require 'mysql2'
-client = Mysql2::Client.new(host: "mysql_host", username: "root", password: "labgoal", database: "app_development")
-sql = "CREATE TABLE IF NOT EXISTS visits(id INT PRIMARY KEY AUTO_INCREMENT,created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP)"
-client.query(sql)
